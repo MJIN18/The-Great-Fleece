7edfc21ca6cfeb47a757880d56651317b35cfffc
Mehul Jain <mehulj16092001@gmail.com>
2026-08-12 22:20:48 +0530

    final commit

--- Assets/The Great Fleece/Game/Scripts/VoiceOverTrigger.cs
+++ Assets/The Great Fleece/Game/Scripts/VoiceOverTrigger.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 public class VoiceOverTrigger : MonoBehaviour
 {
     [SerializeField] private AudioClip _audioClip;
+    private BoxCollider boxCollider;
+
+    private void Start()
+    {
+        boxCollider = GetComponent<BoxCollider>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            boxCollider.gameObject.SetActive(false);
             AudioManager.Instance.PlayVoiceOver(_audioClip);
         }
     }
